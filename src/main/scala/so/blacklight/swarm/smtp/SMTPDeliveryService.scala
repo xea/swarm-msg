61e7c555aaff2c227e393b205b41692e655bfe1f
Alex Pecsi <alex.pecsi@gmail.com>
2017-03-21 19:54:02 +0000

    Created SMTPDeliverySession class

--- src/main/scala/so/blacklight/swarm/smtp/SMTPDeliveryService.scala
+++ src/main/scala/so/blacklight/swarm/smtp/SMTPDeliveryService.scala
@@ -2,6 +2,7 @@ package so.blacklight.swarm.smtp
 
 import akka.actor.{Actor, Props}
 import akka.event.Logging
+import so.blacklight.swarm.mail.Email
 
 /**
 	*
@@ -17,7 +18,14 @@ class SMTPDeliveryService extends Actor {
 	override def receive: Receive = {
 		case DeliverMessage(email) =>
 			logger.info("Begin message delivery")
+			deliverMessages(Stream(email))
 		case _ => ()
+	}
+
+	private def deliverMessages(messages: Stream[Email]): Unit = {
+		val deliverySession = context.actorOf(SMTPDeliverySession.props)
+
+		deliverySession ! 0
 	}
 }
 
@@ -26,3 +34,13 @@ object SMTPDeliveryService {
 }
 
 case class DeliveryConfig(remoteHost: String, remotePort: Int, forceTLS: Boolean)
+
+class SMTPDeliverySession extends Actor {
+	override def receive: Receive = {
+		case _ =>
+	}
+}
+
+object SMTPDeliverySession {
+	def props: Props = Props(new SMTPDeliverySession)
+}
